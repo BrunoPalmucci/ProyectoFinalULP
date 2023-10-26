3e88d17f81e2783f372060c5bedcf7f25b240928
BrunoPalmucci <brunoapalmucci@sanluis.edu.ar>
2023-10-26 16:11:29 -0300

    Update compra proveedor

--- ProyectoFinalULP/src/Entidades/Compra.java
+++ ProyectoFinalULP/src/Entidades/Compra.java
@@ -5,10 +5,62 @@
  */
 package Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Bruno
  */
 public class Compra {
+    private int idCompra;
+    Proveedor proveedor;
+    LocalDate fecha;
+
+    public Compra(int idCompra, Proveedor proveedor, LocalDate fecha) {
+        this.idCompra = idCompra;
+        this.proveedor = proveedor;
+        this.fecha = fecha;
+    }
+
+    public Compra(Proveedor proveedor, LocalDate fecha) {
+        this.proveedor = proveedor;
+        this.fecha = fecha;
+    }
+
+    public Compra() {
+    }
+    
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" + "idCompra=" + idCompra + ", proveedor=" + proveedor + ", fecha=" + fecha + '}';
+    }
+    
+    
+    
     
 }
